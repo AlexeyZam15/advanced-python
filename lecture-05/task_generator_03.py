d973ba53b9dbced0366dfff9344625d7defd2b6e
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-10-21 17:31:54 +1000

    added lecture-05

---
+++ lecture-05/task_generator_03.py
@@ -0,0 +1,14 @@
+x = [1, 1, 2, 3, 5, 8, 13]
+y = [1, 2, 6, 24, 120, 720]
+print(f'{len(x)=}\t{len(y)=}')
+mult = (i + j for i in x if i % 2 != 0 for j in y if j != 1)
+res = list(mult)
+print(f'{len(res)=}\n{res}')
+
+mult = (i + j for i, j in zip(x, y) if i % 2 != 0)
+print(next(mult))
+print(list(mult))
+
+mult = zip(x, y)
+print(next(mult))
+print(list(mult))
---
+++ lecture-05/task_iterator_01.py
@@ -0,0 +1,8 @@
+data = [2, 4, 6, 8]
+list_iter = iter(data)
+print(list_iter)
+
+data = [2, 4, 6, 8]
+list_iter = iter(data)
+print(*list_iter)
+print(*list_iter)
